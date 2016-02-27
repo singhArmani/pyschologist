e777260eed18329e33eb20deef02db945992959e
Aman Singh <singh_aman@hotmail.com.au>
2016-02-28 06:54:27 +1100

    add a test comment

--- Pyschologist/PsychologistViewController.swift
+++ Pyschologist/PsychologistViewController.swift
@@ -10,14 +10,29 @@ import UIKit
 
 class PsychologistViewController: UIViewController {
 
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    @IBAction func nothing(sender: UIButton) {
+        
+        performSegueWithIdentifier("nothing", sender: nil)
+        
+    }
+    //Adding a test commit for git purpose
     
-        if let hvc = segue.destinationViewController as? HappinessViewController{
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        
+        var destination = segue.destinationViewController as UIViewController
+        
+        if let navCon = destination as? UINavigationController {
+            destination = navCon.visibleViewController!  // pulling out the top most visible controller out of the stack
+        }
+    
+        if let hvc = destination as? HappinessViewController{
             if let identifier = segue.identifier {
                 switch identifier {
                 case "sad": hvc.currentResultStorage = "sad"
                 case "happy": hvc.currentResultStorage = "happy"
                 case "naah": hvc.currentResultStorage = "neutral"
+                case "nothing": hvc.currentResultStorage = "i am scared"
                 default: break
                 }
             }
